fdeb73d40233a678d11bde99f56ceda8663ef016
Fatih Mehmet Babacan <f.mehmetbabacan@gmail.com>
2026-09-01 19:19:02 +0300

    Update FolioFold to 0.4.0

--- Casks/foliofold.rb
+++ Casks/foliofold.rb
@@ -1,7 +1,6 @@
 cask "foliofold" do
-  version "0.3.0"
-  sha256 arm:   "5a92eee18ff9844bec2c8c3a3f5d789b3c0062d7a4a95aac170ebc7e8b1e86b0",
-         intel: "9b354e65a3aa1b5792ac8e0ce5617fad0dae1ac99064561ddf1e52c3021b5a06"
+  version "0.4.0"
+  sha256 arm: "091d9069f6554df43f63f1229bc3e710e4f87eb71b7ffda7072bf9acbe3df000", intel: "4c0a845cd5e885ae4ef25217a52365f73289abe1f79ed35bbf3edc5a94fb685e"
 
   on_arm do
     url "https://github.com/fmbabacan/FolioFold/releases/download/v#{version}/FolioFold-#{version}-arm64.zip"
@@ -18,8 +17,6 @@ cask "foliofold" do
     url :url
     strategy :github_latest
   end
-
-  depends_on macos: :sequoia
 
   app "FolioFold.app"
 
